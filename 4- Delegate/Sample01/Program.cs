6c0739f6b36630aaae34d18cd23c846be69ebbe6
Mahta Khoobi <65153341+mahta-khoobi@users.noreply.github.com>
2020-07-18 13:21:41 +0430

    Add files via upload

---
+++ 4- Delegate/Sample01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LifeCycle.Calculator ref_Calculator = new LifeCycle.Calculator(12,3);
+
+        }
+    }
+}
