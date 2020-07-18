ad7866decf994da6567a1f97f5fbacf7693a6f39
Mahta Khoobi <65153341+mahta-khoobi@users.noreply.github.com>
2020-07-18 13:24:52 +0430

    Add files via upload

---
+++ 1- InterfaceDesignPatterns/Sample03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lifecycle.Teacher ref_Teacher = new Lifecycle.Teacher("ALi","Amiri",2000000);
+            Console.WriteLine(ref_Teacher.ShowFullInfo());
+
+            Lifecycle.Student ref_Student = new Lifecycle.Student("Mahsa","Mahmoudi");
+            Console.WriteLine(ref_Student.ShowFullInfo());
+        }
+    }
+}
